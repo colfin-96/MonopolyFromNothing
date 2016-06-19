953cca5a41af311419210c970a1b63cb663ba3b7
DAlexander1 <yellowpineapple@hotmail.co.uk>
2016-06-19 21:48:44 +0100

    Updated BO.

--- MonopolyBO/MonopolyBO/Board.cs
+++ MonopolyBO/MonopolyBO/Board.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace MonopolyBO
 {
-    public class Class1
+    class Board
     {
+
     }
 }
---
+++ MonopolyBO/MonopolyBO/Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyBO
+{
+    class Dice
+    {
+
+        public int Roll()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 13); // creates a number between 1 and 12 (2 dice)
+        }
+    }
+}
