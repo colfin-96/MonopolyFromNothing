5210f1383973052ed4a37a135feeec7abc71d455
DAlexander1 <yellowpineapple@hotmail.co.uk>
2016-06-20 18:06:36 +0100

    Added Player and Property Classes.

---
+++ MonopolyBO/MonopolyBO/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyBO
+{
+    class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Token { get; set; }
+        public int Money { get; set; }
+        public int Position { get; set; }
+        public bool IsInJail {get{if (Position == 10)return true;return false;}}
+
+        public int[] Properties { get; set; }
+        public int[] Cards { get; set; }
+
+
+
+
+
+        public void Play()
+        {
+            if (IsInJail)
+                PlayJail();
+            else
+            {
+
+            }
+        }
+
+        private void PlayJail()
+        {
+
+        }
+
+    }
+}
---
+++ MonopolyBO/MonopolyBO/Property.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyBO
+{
+    class Property
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public int Mortgage { get; set; }
+        public bool IsMortgaged { get; set; }
+        public int Owner { get; set; }
+        public bool IsOwned { get{if (Owner == 0)return false;return true;}}
+        public int NoHouses { get; set; }
+        public int CostOfHouse { get; set; }
+        public string Colour { get; set; }
+
+
+
+    }
+}
