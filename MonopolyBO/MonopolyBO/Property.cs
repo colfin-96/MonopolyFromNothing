8bdb19682c3421e4575edb03656a84eb68d067b2
colfin_96 <fing.co2009@gmail.com>
2016-06-20 20:13:33 +0200

    Revert "You're such noobs"
    This reverts commit b9c2a1c9317210edcf4850addc89cf76375e035e.

--- MonopolyBO/MonopolyBO/Property.cs
+++ MonopolyBO/MonopolyBO/Property.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 
 namespace MonopolyBO
 {
-    class Property : Square
+    class Property
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Value { get; set; }
         public int Mortgage { get; set; }
         public bool IsMortgaged { get; set; }
-        public int OwnerId { get; set; }
-        public bool IsOwned { get { if (OwnerId == -1) return false; return true; } }
+        public int Owner { get; set; }
+        public bool IsOwned { get{if (Owner == 0)return false;return true;}}
         public int NoHouses { get; set; }
         public int CostOfHouse { get; set; }
         public string Colour { get; set; }
+
+
+
     }
 }
